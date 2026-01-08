b26a01b6d8506a71e34b98dfeef7f489efdab7a6
Mynk08 <158840243+Mynk08@users.noreply.github.com>
2026-01-09 02:10:30 +0530

    feat: add kernel/proc.c

---
+++ kernel/proc.c
@@ -0,0 +1,50 @@
+// Process management
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
+
+struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+static struct proc *initproc;
+
+int nextpid = 1;
+
+void pinit(void) {
+  initlock(&ptable.lock, "ptable");
+}
+
+// Look in the process table for an UNUSED proc.
+static struct proc* allocproc(void) {
+  struct proc *p;
+  char *sp;
+
+  acquire(&ptable.lock);
+
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if(p->state == UNUSED)
+      goto found;
+
+  release(&ptable.lock);
+  return 0;
+
+found:
+  p->state = EMBRYO;
+  p->pid = nextpid++;
+
+  release(&ptable.lock);
+
+  // Allocate kernel stack.
+  if((p->kstack = kalloc()) == 0){
+    p->state = UNUSED;
+    return 0;
+  }
+
+  return p;
+}
